d5fa3a42fe13abd215d21e2bff81cd15f55a7280
Samuel Chien <samuel@spotnana.com>
2024-01-08 11:54:45 -0800

    check in

---
+++ main.py
@@ -0,0 +1,410 @@
+#Not Supported City
+        # "Dong Da", 
+        # "Hoang Mai", 
+        # "Hai Ba Trung", 
+        # "Long Bien", 
+        # "Tay Ho", 
+        # "Gia Lam", 
+        # "Ba Dinh", 
+        # "Thanh Tri", 
+        # "Hoan Kiem", 
+        # "Hoai Duc", 
+        # "Dong Anh", 
+        # "Dan Phuong", 
+        # "Chuong My", 
+        # "Quoc Oai", 
+        # "Thach That", 
+        # "Thanh Oai", 
+        # "Me Linh", 
+        # "Soc Son", 
+        # "My Duc", 
+        # "Phuc Tho"
+
+
+
+area = {
+        "Tu Liem": 
+            {
+            "Dong Ngac": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/dong-ngac",
+                "coordinate": "21.088694, 105.786452"
+                },
+            "My Dinh":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/my-dinh",
+                    "coordinate": "21.028513, 105.771172"
+                },    
+            "Tay Mo":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/tay-mo",
+                    "coordinate": "21.010933, 105.745739"
+                },
+            "Xuan Dinh":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/xuan-dinh",
+                    "coordinate": "21.072897, 105.789864"
+                }, 
+            "Me Tri":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/me-tri",
+                    "coordinate": "21.006558, 105.781313"
+                },
+            "Dai Mo":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/dai-mo",
+                    "coordinate": "20.993855, 105.761025"
+                }, 
+            "Co Nhue":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/co-nhue",
+                    "coordinate": "21.060442, 105.778135"
+                },
+            "Cau Dien":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/cau-dien",
+                    "coordinate": "21.032385, 105.761768"
+                }, 
+            "Trung Van":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/trung-van",
+                    "coordinate": "20.990024, 105.786327"
+                },
+            "Xuan Phuong":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/xuan-phuong",
+                    "coordinate": "21.029426, 105.746680"
+                }, 
+            "Minh Khai":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/minh-khai",
+                    "coordinate": "20.995983, 105.857067"
+                },
+            "Phu Dien":
+                {
+                    "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/tu-liem/phu-dien",
+                    "coordinate": "21.047971, 105.764834"
+                }, 
+               
+            }, 
+        "Ha Dong": 
+            {
+            "La Khe": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/la-khe",
+                "coordinate": "20.968915, 105.760427"
+                },
+            "Van Quan": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/van-quan",
+                "coordinate": "20.979019, 105.791318"
+                },
+            "Duong Noi": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/duong-noi",
+                "coordinate": "20.981718, 105.748660"
+                },
+            "Van Phuc": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/van-phuc",
+                "coordinate": "20.979092, 105.772902"
+                },
+            "Phuc La": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/phuc-la",
+                "coordinate": "20.968119, 105.787610"
+                },
+            "Kien Hung": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/kien-hung",
+                "coordinate": "20.951965, 105.784734"
+                },
+            "Mo Lao": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/mo-lao",
+                "coordinate": "20.982394, 105.783578"
+                },
+            "Ha Cau": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/ha-cau",
+                "coordinate": "20.963799, 105.778685"
+                },
+            "Quang Trung": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/quang-trung",
+                "coordinate": "20.968036, 105.771415"
+                },
+            "Yen Nghia": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/yen-nghia",
+                "coordinate": "20.952475, 105.738569"
+                },
+            "Phu La": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/phu-la",
+                "coordinate": "20.957127, 105.765294"
+                },
+            "Nguyen Trai": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/nguyen-trai",
+                "coordinate": "20.969178, 105.779467"
+                },
+            "Phu Lam": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/phu-lam",
+                "coordinate": "20.943682, 105.753096"
+                },
+            "Phu Luong": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/phu-luong",
+                "coordinate": "20.940530, 105.769157"
+                },
+            "Yet Kieu": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/yet-kieu",
+                "coordinate": "20.974721, 105.778043"
+                },
+            "Dong Mai": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/dong-mai",
+                "coordinate": "20.928533, 105.739347"
+                },
+            "Bien Giang": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/ha-dong/bien-giang",
+                "coordinate": "20.930783, 105.721001"
+                },
+            }, 
+        
+        "Cau Giay": 
+            {
+            "Trung Hoa": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/trung-hoa",
+                "coordinate": "21.009694, 105.799093"
+                },
+            "Yen Hoa": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/yen-hoa",
+                "coordinate": "21.021710, 105.793555"
+                },
+            "Dich Vong Hau": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/dich-vong-hau",
+                "coordinate": "21.035437, 105.786839"
+                },
+            "Nghia Do": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/nghia-do",
+                "coordinate": "21.045559, 105.802377"
+                },
+            "Quan Hoa": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/quan-hoa",
+                "coordinate": "21.034829, 105.800443"
+                },
+            "Dich Vong": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/dich-vong",
+                "coordinate": "21.033636, 105.792406"
+                },
+            "Mai Dich": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/mai-dich",
+                "coordinate": "21.040206, 105.775345"
+                },
+            "Nghia Tan": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/nghia-tan",
+                "coordinate": "21.045246, 105.790674"
+                },
+            }, 
+        
+        "Thanh Xuan": 
+            {
+            "Nhan Chinh": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/nhan-chinh",
+                "coordinate": "21.004031, 105.804393"
+                },
+            "Khuong Trung": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/khuong-trung",
+                "coordinate": "20.997257, 105.820862"
+                },
+            "Thuong Dinh": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/thuong-dinh",
+                "coordinate": "21.000317, 105.815635"
+                },
+            "Khuong Mai": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/khuong-mai",
+                "coordinate": "20.997097, 105.830956"
+                },
+            "Thanh Xuan Trung": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/thanh-xuan-trung",
+                "coordinate": "20.996591, 105.805496"
+                },
+            "Thanh Xuan Nam": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/thanh-xuan-nam",
+                "coordinate": "20.986850, 105.798900"
+                },
+            "Ha Dinh": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/ha-dinh",
+                "coordinate": "20.988510, 105.810828"
+                },
+            "Phuong Liet": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/phuong-liet",
+                "coordinate": "20.993338, 105.838372"
+                },
+            "Khuong Dinh": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/khuong-dinh",
+                "coordinate": "20.989063, 105.818156"
+                },
+            "Kim Giang": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/kim-giang",
+                "coordinate": "20.982494, 105.813317"
+                },
+            "Thanh Xuan Bac": 
+                {
+                "link": "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/thanh-xuan/thanh-xuan-bac",
+                "coordinate": "20.992993, 105.799064"
+                },
+            },  
+    }
+
+import googlemaps
+import geopy.distance
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+def get_geo_coordinates(input_address):
+    google_api="AIzaSyCyS3t6e6K9y4HbExqh923AdaJcT_GvHRE"
+    gmaps = googlemaps.Client(key=google_api)
+    geocode_result = gmaps.geocode(input_address)    
+    return geocode_result[0]["geometry"]["location"]["lat"], geocode_result[0]["geometry"]["location"]["lng"]
+
+
+def getDistrictNeighborhoodByAddress(inputAddress):
+    #input_address = "Lô 3A, Số 15, P. Thâm Tâm, Khu đô thị Nam Trung Yên, Cầu Giấy, Hà Nội, Vietnam"
+    input_cor = get_geo_coordinates(input_address)
+    min_distance = 99999
+    min_district = ""
+    min_neighborhood = ""
+    min_link = ""
+    for district in area:
+        for neighborhood in area[district]:
+            corStrList = area[district][neighborhood]["coordinate"].split(", ")
+            cur_cor = (float(corStrList[0]), float(corStrList[1]))
+            
+            cur_min = geopy.distance.geodesic(input_cor, cur_cor).km
+            if cur_min < min_distance:
+                min_distance = cur_min
+                min_district = district
+                min_neighborhood = neighborhood
+                min_link = area[district][neighborhood]["link"]
+
+    return min_district, min_neighborhood, min_link
+
+def getCompDetailListByTargetAddressAndSquareMeter(input_square_meter_target, input_property_type, inputURL):
+    #input_square_meter_target = 100
+    #inputURL = "https://www.fazwaz.vn/property-for-sale/vietnam/hanoi/cau-giay/trung-hoa"
+    pendingURL = "?order_by=user_updated_at|desc&page="
+    page_id = 1
+    end_page_index = 10
+    maximum_result_len = 5
+    target_meter_variation = 0.2
+    
+    
+    hasNextPage = True
+    result = []
+    
+    
+        
+    while hasNextPage:
+        
+        currentURL = inputURL + pendingURL +str(page_id)
+        req = Request(
+            url=currentURL, 
+            headers={'User-Agent': 'Mozilla/5.0'}
+        )
+        webpage = urlopen(req).read()
+        soup = BeautifulSoup(webpage)
+        mydivs = soup.find_all("div", {"class": "result-search__item"})
+    
+        for div in mydivs:
+            property_detail = div.find("div", {"class": "wrap-icon-info"}).text.lstrip().split()
+            if len(property_detail) < 9:
+                continue
+            
+            listing_link = div.find("a", {"class": "link-unit"})["href"]
+            price_detail = div.find("div", {"class": "price-tag"}).text.lstrip().split()
+            price = price_detail[0]
+            square_foot_price = price_detail[1]
+            bedroom = property_detail[0]
+            bathroom = property_detail[2]
+            square_foot = property_detail[4]
+            property_type = property_detail[8]
+
+            if (property_type=="" or property_type == input_property_type) and float(square_foot) > input_square_meter_target*(1-target_meter_variation) and float(square_foot) < input_square_meter_target * (1+target_meter_variation):
+                result.append({
+                        "listing_link":listing_link, 
+                        "price":price,
+                        "square_foot_price":square_foot_price,
+                        "square_foot":square_foot,
+                        "bedroom":bedroom,
+                        "bathroom":bathroom,
+                        "property_type":property_type,
+                    })
+        
+        page_id += 1
+        if end_page_index < page_id or len(mydivs) == 0 or len(result)>maximum_result_len: 
+            hasNextPage = False
+        
+    return result
+
+if __name__ == "__main__":
+    input_square_meter_target = 60
+    input_property_type = ""
+    input_address = "11 LK12, kđt Phú Lương, Hà Đông, Hà Nội, Vietnam"
+    
+    district, neighborhood, inputURL = getDistrictNeighborhoodByAddress(input_address)    
+    compDetailList = getCompDetailListByTargetAddressAndSquareMeter(input_square_meter_target, input_property_type, inputURL)
+    
+    totalDollar = 0
+    totalSquareMeter = 0
+    for comp in compDetailList:
+        totalDollar += float(comp["price"].replace(',', '').replace('$', ''))
+        totalSquareMeter += float(comp["square_foot"])
+    
+    print("District:" + district)
+    print("Neighborhood:" + neighborhood)
+    print("Average Square Footage: $" + str(totalDollar/totalSquareMeter))
+    for comp in compDetailList:
+        print('------------------')
+        print("listing_link:" + comp["listing_link"])
+        print("price:" + comp["price"])
+        print("square_foot_price:" + comp["square_foot_price"])
+        print("square_foot:" + comp["square_foot"])
+        print("bedroom:" + comp["bedroom"])
+        print("bathroom:" + comp["bathroom"])
+        print("property_type:" + comp["property_type"])
+    
+
+        
+        
+        
+            
+            
+       
+        
+            
+
+    
